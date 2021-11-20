2f82f443c29514660a87f974a3e3f14ad6b1cce3
youngkwon02 <youngkwon02@naver.com>
2021-11-21 04:09:06 +0900

    feat: resultPOST router and processing

--- functions/api/routes/result/index.js
+++ functions/api/routes/result/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/character', require('./character'));
-router.use('/result', require('./result'));
+router.post('', require('./resultPOST'));
 
 module.exports = router;
