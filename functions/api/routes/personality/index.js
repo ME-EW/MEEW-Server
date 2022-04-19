d98657fdabd7acb91dfebefe69b9fdd03da1cf51
youngkwon02 <youngkwon02@naver.com>
2022-04-20 02:11:45 +0900

    chore: connect personalityNewGET with the router

--- functions/api/routes/personality/index.js
+++ functions/api/routes/personality/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/today', require('./personalityTodayGET'));
+router.get('/new', require('./personalityNewGET'));
 
 module.exports = router;
