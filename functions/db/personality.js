f643411821b10e6c1974e5f0c0b81e66d09e756b
youngkwon02 <youngkwon02@naver.com>
2022-04-20 02:57:36 +0900

    feat: implement dbFunctions for personalityNewGET

--- functions/db/personality.js
+++ functions/db/personality.js
@@ -1,3 +1,4 @@
+const dayjs = require('dayjs');
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
 const getRecentCharacterById = async (client, userId) => {
@@ -35,8 +36,35 @@ const getCharacterByPersonalityId = async (client, personalityId) => {
   return convertSnakeToCamel.keysToCamel(rows[0]);
 };
 
+const getTasksByPersonalityId = async (client, personalityId) => {
+  const { rows } = await client.query(
+    `
+      SELECT * FROM public.task
+      WHERE personality_id = $1
+    `,
+    [personalityId],
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
+};
+
+const updateRecentCharacter = async (client, userId, personalityId, allTask) => {
+  const now = dayjs().add(9, 'hour');
+  const { rows } = await client.query(
+    `
+      UPDATE public.history
+      SET personality_id = $2, all_task = $3, complete_task = '', updated_at = $4
+      WHERE id = (SELECT id FROM public.history WHERE user_id = $1 ORDER BY created_at DESC LIMIT 1)
+      RETURNING *
+    `,
+    [userId, personalityId, allTask, now],
+  );
+  return convertSnakeToCamel.keysToCamel(rows[0]);
+};
+
 module.exports = {
   getRecentCharacterById,
   getTaskByTaskId,
   getCharacterByPersonalityId,
+  getTasksByPersonalityId,
+  updateRecentCharacter,
 };
