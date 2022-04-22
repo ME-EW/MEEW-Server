4663b272bbd323ce2cc9a60197b13ff3075c351e
youngkwon02 <youngkwon02@naver.com>
2022-04-22 16:11:24 +0900

    feat: add imageUrl response at personalityNewGET API

--- functions/api/routes/personality/personalityNewGET.js
+++ functions/api/routes/personality/personalityNewGET.js
@@ -44,6 +44,7 @@ module.exports = async (req, res) => {
     const updatedUser = await userDB.updateChanceByUserId(client, userId, user.chance - 1);
     const recentHistory = await personalityDB.updateRecentHistory(client, userId, newPersonalityId, newTasks.map((t) => t.id).join());
     const character = await personalityDB.getCharacterByPersonalityId(client, recentHistory.personalityId);
+    const imageUrl = await personalityDB.getImageByLevelAndId(client, 0, recentHistory.personalityId);
 
     let todo = [];
     newTasks.forEach((t) => {
@@ -58,6 +59,7 @@ module.exports = async (req, res) => {
       nickname: updatedUser.nickname,
       name: character.name,
       level: 0,
+      imageUrl,
       chance: updatedUser.chance,
       todo,
     };
