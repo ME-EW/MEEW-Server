d73fe410093e1e1431d4d5c46ede4dd6ae888453
youngkwon02 <youngkwon02@naver.com>
2022-04-22 16:10:23 +0900

    feat: add imageUrl response at personalityCheckPATCH API

--- functions/api/routes/personality/personalityCheckPATCH.js
+++ functions/api/routes/personality/personalityCheckPATCH.js
@@ -49,7 +49,8 @@ module.exports = async (req, res) => {
       completeTaskIds = completeTaskIds.filter((t) => t !== taskId);
     }
 
-    await personalityDB.updateTODO(client, userId, completeTaskIds.join());
+    const updatedHistory = await personalityDB.updateTODO(client, userId, completeTaskIds.join());
+    const imageUrl = await personalityDB.getImageByLevelAndId(client, completeTaskIds.length, updatedHistory.personalityId);
 
     let todo = [];
     for (let i = 0; i < allTaskIds.length; i++) {
@@ -68,6 +69,7 @@ module.exports = async (req, res) => {
       nickname: user.nickname,
       name: character.name,
       level: completeTaskIds.length,
+      imageUrl,
       chance: user.chance,
       todo,
     };
