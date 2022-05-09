41d7dab0bae49fd34b902bab201b879fa73a6239
안준영 <zxzimin@naver.com>
2022-05-09 19:56:13 +0900

    fix: catch conflict nickname exception

--- functions/api/routes/user/userSignUpPOST.js
+++ functions/api/routes/user/userSignUpPOST.js
@@ -17,7 +17,7 @@ module.exports = async (req, res) => {
   try {
     client = await db.connect(req);
 
-    const user = userDB.checkAvailableName(client, nickname);
+    const user = await userDB.checkAvailableName(client, nickname);
     if (user) {
       return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_EXIST_NICKNAME));
     }
@@ -35,7 +35,14 @@ module.exports = async (req, res) => {
 
     request(options, async function(error, response, body){
       const { id: socialId } = JSON.parse(response.body);
-      const user = await userDB.createNewUser(client, socialType, socialId, nickname, personalityId);
+      let user;
+      try {
+        user = await userDB.createNewUser(client, socialType, socialId, nickname, personalityId);
+      } catch (error) {
+        functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+        console.log(error);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+      }
       res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGN_UP_SUCCESS, await jwtUtil.createJwtValue(user.id)));
     });
 
